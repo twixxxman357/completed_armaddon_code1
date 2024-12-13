43c1768f3784dd3a2393037fdff72df0a2fd2c17
twixxxman357 <cloudpro357@gmail.com>
2024-12-13 11:30:19 +0000

    Update 13-tgw.tf

--- 13-tgw.tf
+++ 13-tgw.tf
@@ -1,231 +1,204 @@
-////////////////////////////////////////////////////////////////////////////
+#############################################
+# Transit Gateway Setup for Tokyo and New York
+#############################################
 
-# Provision our Tokyo Transit Gateway 
-
-resource "aws_ec2_transit_gateway" "tokyo-tgw01" {
-  description = "tokyo-tgw01"
+# Create the Tokyo Transit Gateway
+resource "aws_ec2_transit_gateway" "tokyo_tgw" {
+  provider = aws.tokyo
+  description = "Transit Gateway in Tokyo (Hub)"
   tags = {
-    Name = "MAIN TGW tokyo-tgw01"
+    Name = "tokyo-tgw"
   }
 }
 
-
-/////////////////////////////////////////////////////////////////////////////
-
-
-# Attach tokyo-vpc to the tokyo-tgw01 transit gateway
-
- resource "aws_ec2_transit_gateway_vpc_attachment" "tokyo-tgw01_attachment" {
-   provider            = aws.tokyo
-   transit_gateway_id  = aws_ec2_transit_gateway.tokyo-tgw01.id
-   vpc_id              = aws_vpc.tokyo-vpc.id
-   subnet_ids          = [aws_subnet.tokyo-public-subnet-1a.id, aws_subnet.tokyo-private-subnet-1d.id]
-
-   tags = {
-     Name = "a-tokyo-vpc-Prod"
-   }
- }
-
-
- # Attach tokyotest-vpc to the tokyo-tgw01 transit gateway
-
- resource "aws_ec2_transit_gateway_vpc_attachment" "tokyotest-tgw01_attachment" {
-   provider            = aws.tokyo
-   transit_gateway_id  = aws_ec2_transit_gateway.tokyo-tgw01.id
-   vpc_id              = aws_vpc.tokyotest-vpc.id
-   subnet_ids          = [aws_subnet.tokyotest-public-subnet-1a.id, aws_subnet.tokyo-private-subnet-1a.id]
-
-   tags = {
-     Name = "d-tokyotest-vpc-Test"
-   }
- }
-
-////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-#Transit gateway peering connection 
-
-
-
-
-
-#transit gateway peering acceptor
-
-
-#route. Each transit gateway needs 1 routing table
-
-resource "aws_ec2_transit_gateway_route" "example" {
-  destination_cidr_block         = "0.0.0.0/0"
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tokyo-tgw01_attachment.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway.tokyo-tgw01.id
+# Create a route table for the Tokyo TGW
+resource "aws_ec2_transit_gateway_route_table" "tokyo_tgw_rt" {
+  provider          = aws.tokyo
+  transit_gateway_id = aws_ec2_transit_gateway.tokyo_tgw.id
+  tags = {
+    Name = "tokyo-tgw-rt"
+  }
 }
 
-resource "aws_ec2_transit_gateway_route_table" "tokyo-tgw01_rt" {
-  transit_gateway_id = aws_ec2_transit_gateway.tokyo-tgw01.id
+# Attach Tokyo VPC to Tokyo TGW
+resource "aws_ec2_transit_gateway_vpc_attachment" "tokyo_tgw_attachment" {
+  provider = aws.tokyo
+  transit_gateway_id = aws_ec2_transit_gateway.tokyo_tgw.id
+  vpc_id            = aws_vpc.tokyo-vpc.id
+  subnet_ids        = [aws_subnet.tokyo-private-subnet-1a.id, aws_subnet.tokyo-private-subnet-1d.id]
+  
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+
+  tags = {
+    Name = "tokyo-tgw-attachment"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+#############################################
+# New York Transit Gateway
+#############################################
 
-#each route table needs 2 route table associations 
-
-resource "aws_ec2_transit_gateway_route_table_association" "example" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.example.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tokyo-tgw01_rt.id
+# Create the New York Transit Gateway
+resource "aws_ec2_transit_gateway" "newyork_tgw" {
+  provider = aws.new-york
+  description = "Transit Gateway in New York (Spoke)"
+  tags = {
+    Name = "newyork-tgw"
+  }
 }
 
-
-
-#2 actual routes for each route tables
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-/*resource "aws_networkmanager_global_network" "global-network" {
-  description = "Transit Gateway"
+# Create a route table for the New York TGW
+resource "aws_ec2_transit_gateway_route_table" "newyork_tgw_rt" {
+  provider          = aws.new-york
+  transit_gateway_id = aws_ec2_transit_gateway.newyork_tgw.id
+  tags = {
+    Name = "newyork-tgw-rt"
+  }
 }
 
-resource "aws_ec2_transit_gateway" "transit-gateway" {
+# Attach New York VPC to New York TGW
+resource "aws_ec2_transit_gateway_vpc_attachment" "newyork_tgw_attachment" {
+  provider            = aws.new-york
+  transit_gateway_id  = aws_ec2_transit_gateway.newyork_tgw.id
+  vpc_id              = aws_vpc.newyork-vpc.id
+  subnet_ids          = [aws_subnet.newyork-private-subnet-1a.id, aws_subnet.newyork-private-subnet-1b.id]
 
-  auto_accept_shared_attachments  = "enable"
-  default_route_table_association = "enable"
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
 
-  default_route_table_propagation = "enable"
-  transit_gateway_cidr_blocks     = ["10.0.0.0/8"]
+  tags = {
+    Name = "newyork-tgw-attachment"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+#############################################
+# Add data sources for attachments
+#############################################
+
+/*data "aws_ec2_transit_gateway_vpc_attachment" "tokyo_attachment" {
+  provider = aws.tokyo
+  id = aws_ec2_transit_gateway_vpc_attachment.tokyo_tgw_attachment.id
+}
+
+data "aws_ec2_transit_gateway_vpc_attachment" "newyork_attachment" {
+  provider = aws.new-york
+  id = aws_ec2_transit_gateway_vpc_attachment.newyork_tgw_attachment.id
+
+}
+*/
 
 
-resource "aws_networkmanager_transit_gateway_registration" "global-network-tg-registration" {
-  global_network_id   = aws_networkmanager_global_network.global-network.id
-  transit_gateway_arn = aws_ec2_transit_gateway.transit-gateway.arn
+#############################################
+# Route Table Associations
+#############################################
+
+resource "aws_ec2_transit_gateway_route_table_association" "tokyo_tgw_assoc" {
+  provider                        = aws.tokyo
+  transit_gateway_attachment_id   = aws_ec2_transit_gateway_vpc_attachment.tokyo_tgw_attachment.id
+  transit_gateway_route_table_id  = aws_ec2_transit_gateway_route_table.tokyo_tgw_rt.id
+
+  
+  depends_on = [
+    aws_ec2_transit_gateway_vpc_attachment.tokyo_tgw_attachment
+  ]
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "newyork_tgw_assoc" {
+  provider                        = aws.new-york
+  transit_gateway_attachment_id   = aws_ec2_transit_gateway_vpc_attachment.newyork_tgw_attachment.id
+  transit_gateway_route_table_id  = aws_ec2_transit_gateway_route_table.newyork_tgw_rt.id
+
+
+  depends_on = [
+    aws_ec2_transit_gateway_vpc_attachment.newyork_tgw_attachment
+  ]
+}
+
+#############################################
+# TGW Peering Between Tokyo and New York
+#############################################
+
+# Create a peering attachment from New York TGW to Tokyo TGW
+resource "aws_ec2_transit_gateway_peering_attachment" "newyork_to_tokyo_peering" {
+  provider                = aws.new-york
+  transit_gateway_id      = aws_ec2_transit_gateway.newyork_tgw.id
+  peer_transit_gateway_id = aws_ec2_transit_gateway.tokyo_tgw.id
+  peer_region            = "ap-northeast-1"
+
+  tags = {
+    Name = "newyork-to-tokyo-peering"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Accept the peering in Tokyo
+resource "aws_ec2_transit_gateway_peering_attachment_accepter" "tokyo_accepts_newyork_peering" {
+  provider                    = aws.tokyo
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_peering_attachment.newyork_to_tokyo_peering.id
+
+  tags = {
+    Name = "tokyo-accepts-newyork"
+  }
+}
+
+#############################################
+# TGW Routes for Inter-Region Communication
+#############################################
+
+# In Tokyo's TGW route table, add a route to New York's CIDR via the peering attachment
+resource "aws_ec2_transit_gateway_route" "tokyo_to_newyork_route" {
+  provider                       = aws.tokyo
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tokyo_tgw_rt.id
+  destination_cidr_block        = "172.19.0.0/16" # New York VPC CIDR
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_peering_attachment.newyork_to_tokyo_peering.id
+
+  depends_on = [
+    aws_ec2_transit_gateway_peering_attachment_accepter.tokyo_accepts_newyork_peering
+  ]
+}
+
+# In New York's TGW route table, add a route to Tokyo's CIDR via the peering attachment
+resource "aws_ec2_transit_gateway_route" "newyork_to_tokyo_route" {
+  provider                       = aws.new-york
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.newyork_tgw_rt.id
+  destination_cidr_block        = "172.18.0.0/16" # Tokyo VPC CIDR
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_peering_attachment.newyork_to_tokyo_peering.id
+
+  depends_on = [
+    aws_ec2_transit_gateway_peering_attachment_accepter.tokyo_accepts_newyork_peering
+  ]
+}
+
+#############################################
+# VPC Route Table Updates
+#############################################
+
+# Add route in Tokyo private route table to New York CIDR
+/*resource "aws_route" "tokyo_vpc_to_newyork_route" {
+  provider              = aws.tokyo
+  route_table_id        = aws_route_table.tokyo-private.id
+  destination_cidr_block = "172.19.0.0/16"  # New York CIDR
+  transit_gateway_id    = aws_ec2_transit_gateway.tokyo_tgw.id
 }
 
 */
 
-# Work on creating the connections between the Transit Gateway and the VPCs
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/networkmanager_vpc_attachment
-
-# AWS RAM
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ram_resource_association
-
-# Possible Solution
-
-# resource "aws_ec2_transit_gateway" "tgw" {
-#   provider = aws.virginia
-
-#   description = "Transit Gateway connecting Tokyo and Virginia"
-
-#   tags = {
-#     Name = "Main Transit Gateway"
-#   }
-# }
-
-# resource "aws_ec2_transit_gateway_vpc_attachment" "tokyo_tgw_attachment" {
-#   provider            = aws.tokyo
-#   transit_gateway_id  = aws_ec2_transit_gateway.tgw.id
-#   vpc_id              = aws_vpc.tokyo-vpc.id
-#   subnet_ids          = [aws_subnet.tokyo_subnet_1.id, aws_subnet.tokyo_subnet_2.id]
-
-#   tags = {
-#     Name = "Tokyo TGW Attachment"
-#   }
-# }
-
-# resource "aws_ec2_transit_gateway_vpc_attachment" "virginia_tgw_attachment" {
-#   provider            = aws.virginia
-#   transit_gateway_id  = aws_ec2_transit_gateway.tgw.id
-#   vpc_id              = aws_vpc.virginia_vpc.id
-#   subnet_ids          = [aws_subnet.virginia_subnet_1.id, aws_subnet.virginia_subnet_2.id]
-
-#   tags = {
-#     Name = "Virginia TGW Attachment"
-#   }
-# }
-
-# resource "aws_route" "tokyo_to_virginia" {
-#   provider            = aws.tokyo
-#   route_table_id      = aws_route_table.tokyo_route_table.id
-#   destination_cidr_block = aws_vpc.virginia_vpc.cidr_block
-#   transit_gateway_id  = aws_ec2_transit_gateway.tgw.id
-# }
-
-# resource "aws_route" "virginia_to_tokyo" {
-#   provider            = aws.virginia
-#   route_table_id      = aws_route_table.virginia_route_table.id
-#   destination_cidr_block = aws_vpc.tokyo_vpc.cidr_block
-#   transit_gateway_id  = aws_ec2_transit_gateway.tgw.id
-# }
-
-
-
-
-
-
-# resource "aws_networkmanager_core_network" "example" {
-#   global_network_id = aws_networkmanager_global_network.example.id
-
-#   tags = {
-#     "hello" = "world"
-#   }
-# }
-
-# resource "aws_networkmanager_transit_gateway_peering" "example" {
-#   core_network_id     = awscc_networkmanager_core_network.example.id
-#   transit_gateway_arn = aws_ec2_transit_gateway.example.arn
-# }
-
-# resource "aws_networkmanager_vpc_attachment" "example" {
-#   subnet_arns     = aws_subnet.example[*].arn
-#   core_network_id = awscc_networkmanager_core_network.example.id
-#   vpc_arn         = aws_vpc.example.arn
-# }
-
-# resource "aws_networkmanager_connect_attachment" "example" {
-#   core_network_id         = awscc_networkmanager_core_network.example.id
-#   transport_attachment_id = aws_networkmanager_vpc_attachment.example.id
-#   edge_location           = aws_networkmanager_vpc_attachment.example.edge_location
-#   options {
-#     protocol = "GRE"
-#   }
-# }
-
-# resource "aws_networkmanager_connect_peer" "example" {
-#   connect_attachment_id = aws_networkmanager_connect_attachment.example.id
-#   peer_address          = "127.0.0.1"
-#   bgp_options {
-#     peer_asn = 65000
-#   }
-#   inside_cidr_blocks = ["172.16.0.0/16"]
-# }
-
-# resource "aws_networkmanager_transit_gateway_route_table_attachment" "example" {
-#   peering_id                      = aws_networkmanager_transit_gateway_peering.example.id
-#   transit_gateway_route_table_arn = aws_ec2_transit_gateway_route_table.example.arn
-# }
-
-# resource "aws_networkmanager_vpc_attachment" "example" {
-#   subnet_arns     = [aws_subnet.example.arn]
-#   core_network_id = awscc_networkmanager_core_network.example.id
-#   vpc_arn         = aws_vpc.example.arn
-# }
-
-# resource "aws_networkmanager_transit_gateway_route_table_attachment" "example" {
-#   peering_id                      = aws_networkmanager_transit_gateway_peering.example.id
-#   transit_gateway_route_table_arn = aws_ec2_transit_gateway_route_table.example.arn
-# }
+# Add route in New York private route table to Tokyo CIDR
+resource "aws_route" "newyork_vpc_to_tokyo_route" {
+  provider              = aws.new-york
+  route_table_id        = aws_route_table.newyork-private.id
+  destination_cidr_block = "172.18.0.0/16"  # Tokyo CIDR
+  transit_gateway_id    = aws_ec2_transit_gateway.newyork_tgw.id
+}
